8efd55d36f4cb57f282f434221972acb327391f3
Starklord17 <maxi.sm117@gmail.com>
2024-08-13 11:58:28 -0300

    feat: Add route for saving a new book

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,43 @@
 import express from "express";
 import {PORT, mongoDBurl} from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log(req);
   return res.status(200).send('Welcome to the Book Store');
 });
+
+// Route for Save a new Book
+app.post('/books', async (req, res) => {
+  try {
+    if ( 
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({message: 'All fields are required'});
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book); // 201: Created
+    
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({message: error.message});
+  }
+})
+
 
 mongoose
   .connect(mongoDBurl)
