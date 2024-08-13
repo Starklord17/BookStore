20b1ec194fefdce26b78eb5a50b1c058139bdfa7
Starklord17 <maxi.sm117@gmail.com>
2024-08-13 11:39:34 -0300

    chore: Update npm dependencies and add MongoDB connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBurl = 'mongodb+srv://maxsm:BookStore@books-store-mern.4gdfn.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import {PORT} from "./config.js";
+import {PORT, mongoDBurl} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.get('/', (req, res) => {
   console.log(req);
-  return res.status(234).send('Welcome to the Book Store');
+  return res.status(200).send('Welcome to the Book Store');
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBurl)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
