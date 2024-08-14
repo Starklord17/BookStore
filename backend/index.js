23b58c3b06a1279cb1923b5697b5bcdf558afe64
Starklord17 <maxi.sm117@gmail.com>
2024-08-14 18:37:34 -0300

    :zap: Delete a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -90,6 +90,23 @@ app.put('/books/:id', async (req, res) => {
   }
 })
 
+// Route for Delete a Book
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(404).send({message: 'Book not found'});
+    }
+    return res.status(200).send({message: 'Book deleted successfully'});
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({message: error.message});
+  }
+})
+
 // console.log('mongoDBurl:', mongoDBurl);
 
 mongoose
